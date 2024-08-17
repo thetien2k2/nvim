047135aeee2f2d1e17076e65b293fc1e33b7b5f2
Tien Nguyen <thetien2k2@gmail.com>
2024-08-17 09:11:22 +0700

    simplify config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,48 @@
     cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
     build = ":TSUpdate",
     opts = function()
-      return require "configs.treesitter"
+      pcall(function()
+        dofile(vim.g.base46_cache .. "syntax")
+        dofile(vim.g.base46_cache .. "treesitter")
+      end)
+
+      local options = {
+            ensure_installed = {
+              "lua",
+              "luadoc",
+              "printf",
+              "vim",
+              "vimdoc",
+              "go",
+              "gomod",
+              "gosum",
+              "gotmpl",
+              "gowork",
+              "templ",
+              "lua",
+              "typescript",
+              "javascript",
+              "css",
+              "html",
+            },
+            incremental_selection = {
+              enable = true,
+              keymaps = {
+                init_selection = "gnn", -- set to `false` to disable one of the mappings
+                node_incremental = "grn",
+                scope_incremental = "grc",
+                node_decremental = "grm",
+              },
+            },
+        highlight = {
+          enable = true,
+          use_languagetree = true,
+        },
+
+        indent = { enable = true },
+      }
+
+      return options
     end,
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
