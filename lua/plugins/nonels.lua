0998c9c0e0a73848202e53b08add60d8078bc280
Tien Nguyen <thetien2k2@gmail.com>
2025-03-26 09:35:40 +0700

    update

--- lua/plugins/nonels.lua
+++ lua/plugins/nonels.lua
@@ -11,7 +11,7 @@ return {
           null_ls.builtins.diagnostics.golangci_lint,
           null_ls.builtins.formatting.gofumpt,
           null_ls.builtins.formatting.goimports,
-          null_ls.builtins.formatting.prettier,
+          null_ls.builtins.formatting.prettierd,
         },
       }
     end,
