26eb6497b9fb53cb24b3a709a967a03c8aaa04ab
Tien Nguyen <thetien2k2@gmail.com>
2025-04-26 20:42:20 +0700

    replace prettierd

--- lua/setup.lua
+++ lua/setup.lua
@@ -1,2 +1,4 @@
 -- require("luasnip.loaders.from_vscode").lazy_load { paths = { "./snippets" } }
 require('telescope').load_extension('fzf')
+-- vim.diagnostic.config({ virtual_lines = true })
+vim.diagnostic.config({ virtual_text = true })
